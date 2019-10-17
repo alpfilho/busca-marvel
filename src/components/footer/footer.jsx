9cb8558666bc0820e2daea8e14b4d2926bbaef4e
Alfredo Peres <alfredoperes.filho@gmail.com>
2019-10-17 01:06:46 -0300

    Componente de Rodapé

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { FooterContainer, Line } from "./footer.style";
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <Line />
+    </FooterContainer>
+  );
+}
