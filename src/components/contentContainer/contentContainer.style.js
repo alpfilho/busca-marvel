dd285fd8991788cf1505a76b6c0219d1fdb6517e
Alfredo Peres <alfredoperes.filho@gmail.com>
2019-10-17 01:01:24 -0300

    Componente ContentContainer

---
+++ src/components/contentContainer/contentContainer.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled("div")`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled("div")`
+  width: 100%;
+  max-width: 1920px;
+  padding-left: 42px;
+  padding-right: 42px;
+`;
