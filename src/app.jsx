7aea1325a35855fa7161450ddddba0fc5b245e12
Alfredo Peres <alfredoperes.filho@gmail.com>
2019-10-17 13:46:18 -0300

    Correção App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "normalize.css";
 import "./app.style.css";
 
+import { Header } from "./components/header";
+import { Footer } from "components/footer";
+import { Search } from "components/search";
+
+import { Character } from "pages/character";
+import { CharactersList } from "pages/charactersList";
+
 function App() {
-  return <h1>teste</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Search />
+
+      <Switch>
+        {/* Página de personagem */}
+        <Route path="/personagem/:slug">
+          <Character />
+        </Route>
+
+        {/* Roteamento e Exibição de Personagens */}
+        <Route path={["/:page", "/"]}>
+          <CharactersList />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
