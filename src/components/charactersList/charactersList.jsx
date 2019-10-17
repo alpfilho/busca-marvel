06bf64e42cd9851e1d8c9014ce277e20bc497ec1
Alfredo Peres <alfredoperes.filho@gmail.com>
2019-10-17 13:43:16 -0300

    Remoção CharactersList

--- src/components/charactersList/charactersList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { CharactersListContainer } from "./charactersList.style";
-
-export const CharactersList = () => {
-  return <CharactersListContainer></CharactersListContainer>;
-};
